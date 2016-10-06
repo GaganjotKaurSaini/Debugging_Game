80bc553df08c1a7447ea64183f2db4ad3884584d
manpreetwalia <manpreetwalia.2008@rediffmail.com>
2016-10-06 12:22:39 +1100

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,24 +1,22 @@
-public class Dice
-{
+public class Dice {
 			
 	private DiceValue value;
-	// declare various functions
-	public Dice() 
-	{
+	
+	public Dice() {
 		value =  DiceValue.getRandom();
 	}
 	
-	public DiceValue getValue() 
-	{
+	public DiceValue getValue()
+	{		
+		
 		return value;
 	}
 
-	public DiceValue roll() 
-	{
+	public DiceValue roll() {
 		return DiceValue.getRandom(); //return diceValue randomly
 	}		
 	
-	public String toString() 
+	public String toString()  
 	{
 		return value.toString();
 	}

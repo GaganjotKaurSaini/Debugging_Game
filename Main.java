cbd967845161346744cc84735ec1e7b5d10474e5
manpreetwalia <manpreetwalia.2008@rediffmail.com>
2016-10-06 12:22:15 +1100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,37 @@
 import java.util.List;
 import java.io.*;
 
-public class Main
-{
-	
-	public static void main(String[] args) throws Exception 
-	{
+public class Main 
+{	
+	public static void main(String[] args) throws Exception {
 		
 	   BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-
+	   
         Dice d1 = new Dice();
         Dice d2 = new Dice();
         Dice d3 = new Dice();
-
-        Player player = new Player("Fred", 100);
+      
+        
+        Player player = new Player("Fred", 19, 100);
         Game game = new Game(d1, d2, d3);
         List<DiceValue> cdv = game.getDiceValues();
 
         int totalWins = 0;
-        int totalLosses = 0;
-
+        int totalLosses = 0;    
+        //float countWin = 0;
+                
         while (true)
         {
             int winCount = 0;
-            int loseCount = 0;
-            int betMoney = 0;
+            int loseCount = 0;            
             
             for (int i = 0; i < 100; i++)
             {
             	String name = "Fred";
+            	int age = 19;
             	int balance = 100;
             	int limit = 0;
-                player = new Player(name, balance);
+                player = new Player(name, age, balance);
                 player.setLimit(limit);
                 int bet = 5;
 
@@ -40,43 +40,50 @@ public class Main
                 		player.getName(), player.getBalance(), player.getLimit()));
 
                 int turn = 0;
-                while (player.balanceExceedsLimitBy(bet) && player.getBalance() < 200)
+<<<<<<< HEAD
+                								// Eradicated bug sets the limit less than and equals to 100
+                while (player.balanceExceedsLimitBy(bet) && player.getBalance() <= 100)
+=======
+                while (player.balanceExceedsLimitBy(bet) && player.getBalance() <= 100)// Eradicated bug sets the limit 100
+>>>>>>> origin/master
                 {
-                    turn++;                    
+                 //   turn++;                    
                 	DiceValue pick = DiceValue.getRandom();
                    
                 	System.out.printf("Turn %d: %s bet %d on %s\n",
                 			turn, player.getName(), bet, pick); 
+                	turn++;
+                	
+                	//DiceValue pick = DiceValue.getRandom();
                 	
                 	int winnings = game.playRound(player, pick, bet);
-                    cdv = game.getDiceValues();
+                    cdv = game.getDiceValues();                    
                     
                     System.out.printf("Rolled %s, %s, %s\n",
-                    		cdv.get(0), cdv.get(1), cdv.get(2));
+                    		cdv.get(0), cdv.get(1), cdv.get(2));                                       
                     
-        //increasing balance when player wins
-                    
-                    if (winnings > 0) {  
-                    	int inc = betMoney + winnings + player.getBalance(); //Adding winnings ()
+                    //increasing balance when player wins
+                                      
+                    if (winnings > 0)
+                    {                 	 
+                	 int x = bet + winnings + player.getBalance(); //eradicated bug 1, now player balance increases when wins
                     	
 	                    System.out.printf("%s won %d, balance now %d\n\n",
-	                    		player.getName(), winnings, inc);
-	                	winCount++; 
-                    }
-                    else
-		    {
+	                    		player.getName(), winnings, x);
+	                	winCount++;	                	
+                    }                    
+                    else {
                     	
 	                    System.out.printf("%s lost, balance now %d\n\n",
 	                    		player.getName(), player.getBalance());
-	                	loseCount++;
-                    }
+	                	loseCount++;	                		                		                	
+                    }                               
                     
-                } //while
-                  //Getting right winCounts
+                } //while checking bugs
+                  //Eradicate bug 3, Getting right winCounts
                 System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
                 System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
-                
-            } //for
+                }
             
             System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
             totalWins += winCount;
@@ -84,9 +91,9 @@ public class Main
 
             String ans = console.readLine();
             if (ans.equals("q")) break;
-        } //while true
-        
-        System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
+        } 
+        System.out.println(String.format("Overall win rate = %.1f%%", (float)((totalWins  / (totalWins + totalLosses))*100)));
+	   
+        //Win Count Limit
+       	}              			
 	}
-
-}

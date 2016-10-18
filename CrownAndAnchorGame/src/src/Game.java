294b2497dfa72c73dafb88dd5b71d547e19ac389
aamerk2 <jawwadaamer@gmail.com>
2016-10-18 17:54:00 +1100

    Given condition

--- CrownAndAnchorGame/src/src/Game.java
+++ CrownAndAnchorGame/src/src/Game.java
@@ -31,7 +31,7 @@ public class Game {
 	}	
 	
 	public int playRound(Player player, DiceValue pick, int bet ) {		
-		if (player == null) throw new IllegalArgumentException("Player cannot be null.");
+		if (player == null) throw new IllegalArgumentException("Player cannot be null.");//giving condition 
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
 		

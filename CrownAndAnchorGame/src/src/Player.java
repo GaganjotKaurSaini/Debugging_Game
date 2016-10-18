cc1e4f9e64bd86b93e7c6f3253ceee0cd233300f
aamerk2 <jawwadaamer@gmail.com>
2016-10-18 17:56:06 +1100

    Condition for limit

--- CrownAndAnchorGame/src/src/Player.java
+++ CrownAndAnchorGame/src/src/Player.java
@@ -52,7 +52,7 @@ public class Player {
 		return limit; }
 	
 	public void setLimit(int limit) {
-		if (limit < 0) throw new IllegalArgumentException("Limit cannot be negative.");   
+		if (limit < 0) throw new IllegalArgumentException("Limit cannot be negative.");   //Condition of limit cnnot be negative
 		if (limit > balance)  throw new IllegalArgumentException("Limit cannot be greater than balance.");
 		this.limit = limit;
 	}

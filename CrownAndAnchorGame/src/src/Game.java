6d0443316fafc7f104b3d5973c3c9052c105c8eb
aamerk2 <jawwadaamer@gmail.com>
2016-10-18 17:59:02 +1100

    condition for bet

--- CrownAndAnchorGame/src/src/Game.java
+++ CrownAndAnchorGame/src/src/Game.java
@@ -33,7 +33,7 @@ public class Game {
 	public int playRound(Player player, DiceValue pick, int bet ) {		
 		if (player == null) throw new IllegalArgumentException("Player cannot be null.");//giving condition 
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");// condition pick cant be negative
-		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
+		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");// condition bet cannot be negative
 		
 		player.takeBet(bet);
 		
